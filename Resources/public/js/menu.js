314648d07f2c0b9351ec622a506e62cfd25e4c40
inquis <inquis06@rambler.ru>
2012-07-27 13:28:51 +0300

    add class if menu has no children elements

--- Resources/public/js/menu.js
+++ Resources/public/js/menu.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.main_menu > ul > li').click(function(){
-        $(this).addClass('active').siblings('li.active').removeClass('active');
+        $(this).addClass('active').siblings('li.active').removeClass('active');  
+        // add class if menu has no children
+        if ($(this).children('ul').length > 0) {
+            $('body').removeClass('special');
+        } else {
+            $('body').addClass('special');
+        }
     })
 })
