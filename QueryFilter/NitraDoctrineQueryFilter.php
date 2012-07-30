192ef89c77deccc671a77469f996a19cc400a462
vitaliytv <vitaliytv@nitralabs.com>
2012-07-30 19:42:00 +0300

    Используем свой класс для исправления ошибки при фильтрации по списку обьектов (в BETA4)

--- QueryFilter/NitraDoctrineQueryFilter.php
+++ QueryFilter/NitraDoctrineQueryFilter.php
@@ -7,10 +7,13 @@ use Admingenerator\GeneratorBundle\QueryFilter\DoctrineQueryFilter;
 class NitraDoctrineQueryFilter extends DoctrineQueryFilter
 {
 
+    /**
+     * Используем свой метод для исправления ошибки при фильтрации по списку обьектов (в BETA4)
+     */
     public function addEntityFilter($field, $value)
     {
         $this->query->andWhere(sprintf('q.%s = :%s',$field, $field));
-        $this->query->setParameter($field, $value->getId());
+        $this->query->setParameter($field, $this->query->getEntityManager()->getUnitOfWork()->getEntityIdentifier($value));
     }
  
 }
