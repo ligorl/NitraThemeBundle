96ad023ac09f72a0061dfa6171ece16743eb9363
vitaliytv <vitaliytv@nitralabs.com>
2012-08-01 09:31:50 +0300

    стили меню форма логина

--- Resources/public/js/menu.js
+++ Resources/public/js/menu.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     $('.main_menu > ul > li').click(function(){
         $(this).addClass('active').siblings('li.active').removeClass('active');  
-        // add class if menu has no children
+        // add class if menu has submenu
         if ($(this).children('ul').length > 0) {
-            $('body').removeClass('special');
+            $('.main_menu').addClass('has_submenu');
         } else {
-            $('body').addClass('special');
+            $('.main_menu').removeClass('has_submenu');
         }
     })
+    
+    // first display
+    if ($('.main_menu .active').children('ul').length > 0) {
+        $('.main_menu').addClass('has_submenu');
+    }
 })
