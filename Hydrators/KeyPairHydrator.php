1442cd11658ce45bf5c654ad63d0cacdc67e51f4
vitaliytv <vitaliytv@nitralabs.com>
2012-08-12 20:12:42 +0300

    Add KeyPair Hydrator fix links in layout

---
+++ Hydrators/KeyPairHydrator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nitra\NitraThemeBundle\Hydrators;
+
+use Doctrine\ORM\Internal\Hydration\AbstractHydrator;
+
+/**
+ * Ключ значение
+ */
+class KeyPairHydrator extends AbstractHydrator
+{
+    protected function hydrateAllData()
+    {
+        return $this->_stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
+    }
+}
