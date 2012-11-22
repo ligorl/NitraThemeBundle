fa25d0b97658d3fbcc2c706b2c7ade513be9a2b3
AlexandrLubenec <alexander@nitralabs.com>
2012-11-22 16:53:17 +0200

    trait ValidForDelete Проверяет, можно ли удалять объект

---
+++ Traits/ValidForDelete.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Nitra\NitraThemeBundle\Traits;
+
+
+trait ValidForDelete
+{
+    /**
+     * Проверяет, можно ли удалять объект
+     * @return boolean
+     */
+    public function isValidForDelete()
+    {
+        // проверка наличия связей с другими объектами
+        $reflectionClass = new \ReflectionClass($this);
+
+        $return = true;
+       
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            $value = $property->getValue($this);
+            if ($value instanceof \Doctrine\ORM\PersistentCollection) {
+                if ($value->count() > 0) {
+                    $return = false;
+                }
+            }
+        }
+
+        return $return;
+    }
+}
