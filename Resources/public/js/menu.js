a0e2fdc9545b45d86390c660f894bf612f0e712d
v.pisotskaya <v.pisotskaya@nitralabs.com>
2013-08-09 12:03:26 +0300

    change styles

--- Resources/public/js/menu.js
+++ Resources/public/js/menu.js
@@ -1,3 +1,45 @@
+function isBreakPoint(bp) {
+  var bps = [800, 1366];
+  var w = $(window).width();
+  var min, max;
+  for (var i = 0, l = bps.length; i < l; i++) {
+    if (bps[i] === bp) {
+      min = bps[i-1] || 0;
+      max = bps[i];
+      break;
+    }
+  }
+  return w > min && w <= max;
+}
+
+if (isBreakPoint(1366)) {
+  (function($){
+      $(function() { 
+        $('.link').each(function() {
+                var el = $(this);
+                var title = el.text();
+                if (title && title != '') {
+                  el.attr('title', '').append('<div class="title">' + title + '</div>');
+                  el.hover(
+                    function() {
+                      el.find('div')
+                        .clearQueue()
+                        .delay(200)
+                        .fadeIn(200);
+                    },
+                    function() {
+                      el.find('div')
+                        .fadeOut(500);
+                    }
+                  ).mouseleave(function() {
+                    if (el.children().is(':hidden')) el.find('div').clearQueue();
+                  });
+                }
+              });
+      });
+    })(jQuery)
+}
+
 $(document).ready(function(){
     //hide sub_menu on click in another place
     jQuery('html').click(function() { 
